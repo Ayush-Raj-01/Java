eacc55a5c050030f5ffd0f703ed19452c4c0cf9c
Ayush Raj <ayussshhraj@gmail.com>
2025-01-14 09:39:36 +0530

    Literals in Java

---
+++ 03_literals.java
@@ -0,0 +1,22 @@
+class literals {
+    public static void main(String[] args) {
+        byte age = 34;
+        System.out.println(age);
+        int age2 = 56;
+        System.out.println(age2);
+        short age3 = 87;
+        System.out.println(age3);
+        long ageDino = 534849484994L;
+        System.out.println(ageDino);
+        char ch ='A';
+        System.out.println(ch);
+        float f1 = 5.6f;
+        System.out.println(f1);
+        double dl = 4.66;
+        System.out.println(dl);
+        boolean a = true;
+        System.out.println(a);
+        String str = "AYUSH";
+        System.out.println(str);
+    }
+}
