68c352799acb63f537a9b521234c8a2467a454cd
Ayush Raj <ayussshhraj@gmail.com>
2025-01-14 21:38:22 +0530

    Percent calculation

---
+++ 05_percencalculate.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+class Percencalculate {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Total Marks: ");
+        int marks = sc.nextInt();
+        System.out.println("Enter Marks of English:");
+        float a = sc.nextInt();
+        System.out.println("Enter Marks of Maths:");
+        float b = sc.nextInt();
+        System.out.println("Enter Marks of Science:");
+        float c = sc.nextInt();
+        System.out.println("Enter Marks of Social Science:");
+        float d = sc.nextInt();
+        System.out.println("Enter Marks of Hindi:");
+        float e = sc.nextInt();
+        float sum = a+b+c+d+e;
+        int mult = marks*5;
+        float x = (sum*100 / mult);
+        System.out.println("The percentage are:");
+        System.out.println(x); 
+        sc.close();
+    }
+}
