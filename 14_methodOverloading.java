47a1c7dda64d546d01305a902da4819b0a0bb998
Ayush Raj <ayussshhraj@gmail.com>
2025-02-04 23:58:16 +0530

    Method Overloading

---
+++ 14_methodOverloading.java
@@ -0,0 +1,9 @@
+class MethodOverloading {
+
+    static void telljock(){
+        System.out.println("To the guy who invented zero: Thanks for nothing!");
+    }
+public static void main(String[] args) {
+    telljock(); 
+}  
+}
